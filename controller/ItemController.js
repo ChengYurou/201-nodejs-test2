5d81c9e7580b8f94dce35c780a659c3713a3cb6c
chengyurou <18829292695@163.com>
2017-02-15 18:42:02 +0800

    init

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -1,5 +1,7 @@
-export default class ItemController {
+class ItemController {
     getAll(req, res, next) {
         res.send("should return all items");
     }
 }
+
+module.exports = ItemController;
